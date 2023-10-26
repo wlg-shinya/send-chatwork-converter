c10b4a8b4ede1839f03431364ea9bef5cb26a449
s-watanabe <s-watanabe@three-rings.net>
2023-10-26 15:37:57 +0900

    独自メニューが追加されないことがあるもんぢ亜の対処

--- content-scripts.js
+++ content-scripts.js
@@ -7,50 +7,49 @@ function contentsSetup() {
         for (const message of messageNodeList) {
             // アクションメニューはメッセージの子要素として追加されるのでそれを監視
             new MutationObserver(() => {
-                // アクションメニューが見つからない場合は何もしない
-                const messageActionNav = message.querySelector(".messageActionNav")
-                if (!messageActionNav) return
-
-                // 独自メニューの構築と追加
-                const MENU_ID = "link-to-chatconv"
-                if (!document.querySelector(`#${MENU_ID}`)) {
-                    // 必要な既存要素の存在チェック
-                    const moreActionButton = document.querySelector(".moreActionButton")
-                    if (!moreActionButton) throw Error("Not found moreActionButton")
-                    const moreActionButtonParent = moreActionButton.parentElement
-                    if (!moreActionButtonParent) throw Error("Not found moreActionButton's parent")
-                    const actionButton = document.querySelector(".actionButton")
-                    if (!actionButton) throw Error("Not found actionButton")
-                    const actionLabel = document.querySelector(".actionLabel")
-                    if (!actionLabel) throw Error("Not found actionLabel")
-                    // 独自メニュー構築
-                    const label = document.createElement("span")
-                    label.textContent = "コンバータで変換"
-                    const button = document.createElement("button")
-                    button.appendChild(label)
-                    button.onclick = () => {
-                        const messageLink = `https://www.chatwork.com/#!rid${message.getAttribute("data-rid")}-${message.getAttribute("data-mid")}`
-                        console.log(messageLink)
-                        chrome.runtime.sendMessage({
-                            type: 'jumpToChatconv',
-                            arg: messageLink
-                        })
-                            .then(() => {
+                // アクションメニューが見つかるまで待つ
+                waitForElement(".messageActionNav", (messageActionNav) => {
+                    // 独自メニューの構築と追加
+                    const MENU_ID = "link-to-chatconv"
+                    if (!document.querySelector(`#${MENU_ID}`)) {
+                        // 必要な既存要素の存在チェック
+                        const moreActionButton = document.querySelector(".moreActionButton")
+                        if (!moreActionButton) throw Error("Not found moreActionButton")
+                        const moreActionButtonParent = moreActionButton.parentElement
+                        if (!moreActionButtonParent) throw Error("Not found moreActionButton's parent")
+                        const actionButton = document.querySelector(".actionButton")
+                        if (!actionButton) throw Error("Not found actionButton")
+                        const actionLabel = document.querySelector(".actionLabel")
+                        if (!actionLabel) throw Error("Not found actionLabel")
+                        // 独自メニュー構築
+                        const label = document.createElement("span")
+                        label.textContent = "コンバータで変換"
+                        const button = document.createElement("button")
+                        button.appendChild(label)
+                        button.onclick = () => {
+                            const messageLink = `https://www.chatwork.com/#!rid${message.getAttribute("data-rid")}-${message.getAttribute("data-mid")}`
+                            console.log(messageLink)
+                            chrome.runtime.sendMessage({
+                                type: 'jumpToChatconv',
+                                arg: messageLink
                             })
-                            .catch((err) => {
-                                throw err
-                            })
+                                .then(() => {
+                                })
+                                .catch((err) => {
+                                    throw err
+                                })
+                        }
+                        const menu = document.createElement("li")
+                        menu.appendChild(button)
+                        menu.id = MENU_ID
+                        // デザイン統一のため同等の要素からclassを頂く
+                        label.className = actionLabel.className
+                        button.className = actionButton.className
+                        menu.className = moreActionButtonParent.className
+                        // 「…」ボタン前に追加
+                        messageActionNav.insertBefore(menu, moreActionButtonParent)
                     }
-                    const menu = document.createElement("li")
-                    menu.appendChild(button)
-                    menu.id = MENU_ID
-                    // デザイン統一のため同等の要素からclassを頂く
-                    label.className = actionLabel.className
-                    button.className = actionButton.className
-                    menu.className = moreActionButtonParent.className
-                    // 「…」ボタン前に追加
-                    messageActionNav.insertBefore(menu, moreActionButtonParent)
-                }
+                })
             }).observe(message, {
                 childList: true
             })
@@ -60,9 +59,10 @@ function contentsSetup() {
 
 function waitForElement(querySelectorPath, func) {
     const intervalId = setInterval(() => {
-        if (document.querySelector(querySelectorPath) != null) {
+        const element = document.querySelector(querySelectorPath)
+        if (element != null) {
             clearInterval(intervalId)
-            func()
+            func(element)
         }
     }, 100);
 }
