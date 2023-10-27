726fbf66e985489fc150d7f9c561423053d6c000
s-watanabe <s-watanabe@three-rings.net>
2023-10-27 12:04:06 +0900

    UI構築時にエラーが起きたら、ちゃんとキャッチしてUI構築中断するように

--- content-scripts.js
+++ content-scripts.js
@@ -7,44 +7,49 @@ function contentsSetup() {
                 // 独自メニューの構築と追加
                 const MENU_ID = "link-to-chatconv"
                 if (!messageActionNav.querySelector(`#${MENU_ID}`)) { // 構築済みなら何もしない
-                    // 必要な既存要素の存在チェック
-                    const message = messageActionNav.closest("._message")
-                    if (!message) throw Error("Not found message")
-                    const moreActionButton = messageActionNav.querySelector(".moreActionButton")
-                    if (!moreActionButton) throw Error("Not found moreActionButton")
-                    const moreActionButtonParent = moreActionButton.parentElement
-                    if (!moreActionButtonParent) throw Error("Not found moreActionButton's parent")
-                    const actionButton = messageActionNav.querySelector(".actionButton")
-                    if (!actionButton) throw Error("Not found actionButton")
-                    const actionLabel = actionButton.querySelector(".actionLabel")
-                    if (!actionLabel) throw Error("Not found actionLabel")
-                    // 独自メニュー構築
-                    const label = document.createElement("span")
-                    label.textContent = "コンバータで変換"
-                    const button = document.createElement("button")
-                    button.appendChild(label)
-                    button.onclick = () => {
-                        const messageLink = `https://www.chatwork.com/#!rid${message.getAttribute("data-rid")}-${message.getAttribute("data-mid")}`
-                        console.log(messageLink)
-                        chrome.runtime.sendMessage({
-                            type: 'jumpToChatconv',
-                            arg: messageLink
-                        })
-                            .then(() => {
+                    try {
+                        // 必要な既存要素の存在チェック
+                        const message = messageActionNav.closest("._message")
+                        if (!message) throw Error("Not found message")
+                        const moreActionButton = messageActionNav.querySelector(".moreActionButton")
+                        if (!moreActionButton) throw Error("Not found moreActionButton")
+                        const moreActionButtonParent = moreActionButton.parentElement
+                        if (!moreActionButtonParent) throw Error("Not found moreActionButton's parent")
+                        const actionButton = messageActionNav.querySelector(".actionButton")
+                        if (!actionButton) throw Error("Not found actionButton")
+                        const actionLabel = actionButton.querySelector(".actionLabel")
+                        if (!actionLabel) throw Error("Not found actionLabel")
+                        // 独自メニュー構築
+                        const label = document.createElement("span")
+                        label.textContent = "コンバータで変換"
+                        const button = document.createElement("button")
+                        button.appendChild(label)
+                        button.onclick = () => {
+                            const messageLink = `https://www.chatwork.com/#!rid${message.getAttribute("data-rid")}-${message.getAttribute("data-mid")}`
+                            console.log(messageLink)
+                            chrome.runtime.sendMessage({
+                                type: 'jumpToChatconv',
+                                arg: messageLink
                             })
-                            .catch((err) => {
-                                throw err
-                            })
+                                .then(() => {
+                                })
+                                .catch((err) => {
+                                    throw err
+                                })
+                        }
+                        const menu = document.createElement("li")
+                        menu.appendChild(button)
+                        menu.id = MENU_ID
+                        // デザイン統一のため同等の要素からclassを頂く
+                        label.className = actionLabel.className
+                        button.className = actionButton.className
+                        menu.className = moreActionButtonParent.className
+                        // 「…」ボタン前に追加
+                        messageActionNav.insertBefore(menu, moreActionButtonParent)
                     }
-                    const menu = document.createElement("li")
-                    menu.appendChild(button)
-                    menu.id = MENU_ID
-                    // デザイン統一のため同等の要素からclassを頂く
-                    label.className = actionLabel.className
-                    button.className = actionButton.className
-                    menu.className = moreActionButtonParent.className
-                    // 「…」ボタン前に追加
-                    messageActionNav.insertBefore(menu, moreActionButtonParent)
+                    catch {
+                        // 何かしらエラーが起きたらUI構築をあきらめるだけ
+                    }
                 }
             })
         }).observe(messageRoot, {
